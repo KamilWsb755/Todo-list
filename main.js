37a0ed7d4bd6c0d4751c3bbe68efbcb1b9b8bc14
Kamil Trybel <ktrybel@teb-gdansk.pl>
2022-05-08 12:15:23 +0200

    add task and hint how to do removeTask

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
-const input = document.querySelector('input');
+const form = document.querySelector('form');
 const ul = document.querySelector('ul');
-const liElements = document.querySelectorAll('li');
+const taskNumber = document.querySelector('h1 span');
+// const listItems = document.getElementsByClassName('task')
+const input = document.querySelector('input');
 
-const searchTask = (e)=>{
-    const searchText = e.target.value.toLowerCase();
-    let tasks = [...liElements];
-    tasks = tasks.filter(task => task.textContent.toLowerCase().includes(searchText));
-    ul.textContent = "";
-    tasks.forEach(li => ul.appendChild(li));
+
+const removeTask = e=>{
+    console.log(e.target);
 }
 
-input.addEventListener('input',searchTask);
+
+
+const addTask = e =>{
+    e.preventDefault();
+    const titleTask = input.value;
+    
+    if(titleTask === "")  return alert('Zadanie nie może byc puste!');
+    
+    const task = document.createElement('li');
+    task.className = 'task';
+    task.innerHTML = titleTask + "<button>Usuń</button>";
+    ul.appendChild(task);
+    input.value = "";
+    taskNumber.textContent = document.querySelectorAll('.task').length;
+    task.querySelector('button').addEventListener('click',removeTask)
+}
+
+form.addEventListener('submit',addTask)
+
+
+
+// const myLi = document.querySelector('li');
+// const button = myLi.querySelector('button');
+// button.addEventListener('click',(e)=>{
+//     console.log(e.target.parentNode);
+// })
